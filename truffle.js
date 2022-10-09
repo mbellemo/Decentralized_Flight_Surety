6fb4b43aaeb641cac7bd981061ae0dccc6f87177
mbellemo <Cazzo.82>
2022-10-09 16:09:09 +0200

    Add dotenv and change truffle config to protect the mnemonic

--- truffle.js
+++ truffle.js
@@ -1,14 +1,18 @@
 var HDWalletProvider = require("@truffle/hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 4500000
     }
   },
   compilers: {
